e32eac909d5506a4a3070c9a739f4791576d9ec2
Ryan Hageman <ryanhageman@icloud.com>
2025-07-27 14:18:26 -0500

    feat: add lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,78 @@
+local lsp_servers = require("ryan.utils.lsp-servers")
+
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+				component_separators = "|",
+				section_separators = { left = "", right = "" },
+				globalstatus = true,
+				disabled_filetypes = {},
+			},
+
+			tabline = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = { { "tabs", separator = { left = "", right = "" }, left_padding = 2 } },
+			},
+
+			winbar = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = { { "filename", separator = { left = "", right = "" } } },
+				lualine_z = {},
+			},
+
+			inactive_winbar = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = { "filename" },
+				lualine_z = {},
+			},
+
+			sections = {
+				lualine_a = { { "mode", separator = { left = "" }, left_padding = 2 } },
+				lualine_b = {
+					{ "filetype", icon_only = true, separator = { right = "" } },
+					{
+						"filename",
+						path = 4,
+						file_status = true,
+						symbols = {
+							modified = "●",
+						},
+					},
+				},
+				lualine_c = { "location", "progress" },
+				lualine_x = {
+					{
+						lsp_servers,
+					},
+					"diff",
+					"diagnostics",
+				},
+				lualine_y = { { "datetime", style = "%a, %b %d - %I:%M %p" } },
+				lualine_z = { { "branch", separator = { right = "" }, left_padding = 2 } },
+			},
+
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+		})
+	end,
+}
---
+++ lua/ryan/utils/lsp-servers.lua
@@ -0,0 +1,55 @@
+local function null_ls_servers()
+	local null_ls_s, null_ls = pcall(require, "null-ls")
+	local servers = {}
+
+	if null_ls_s then
+		local sources = null_ls.get_sources()
+		local buf_ft = vim.bo.filetype
+
+		for _, source in ipairs(sources) do
+			if source._validated then
+				for ft_name, ft_active in pairs(source.filetypes) do
+					if ft_name == buf_ft and ft_active then
+						table.insert(servers, source.name)
+					end
+				end
+			end
+		end
+	end
+
+	return servers
+end
+
+local function get_attached_lsp_servers()
+	local current_buffer = vim.api.nvim_get_current_buf()
+	local clients = vim.lsp.buf_get_clients(current_buffer)
+	local servers = {}
+
+	for _, client in pairs(clients) do
+		if client.name == "null-ls" then
+			local null_clients = null_ls_servers()
+
+			for _, null_client in pairs(null_clients) do
+				table.insert(servers, null_client)
+			end
+
+			goto continue
+		end
+
+		table.insert(servers, client.name)
+		::continue::
+	end
+
+	return servers
+end
+
+local function lsp_servers()
+	local server_list = get_attached_lsp_servers()
+	if #server_list <= 1 then
+		return table.concat(server_list, ", ")
+	else
+		return "[" .. table.concat(server_list, ", ") .. "]"
+	end
+end
+
+return lsp_servers
