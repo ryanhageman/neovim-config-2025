b6f34ab18e459c116cebcac58230654f76456e92
Ryan Hageman <ryanhageman@icloud.com>
2025-07-27 17:22:10 -0500

    fix: oil toggle keymap

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,6 +4,12 @@ return {
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   lazy = false,
   keys = {
-    vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "Explorer" }),
+    vim.keymap.set("n", "<leader>e", function()
+      if vim.bo.filetype == "oil" then
+        vim.cmd("bd")  -- Close the oil buffer
+      else
+        vim.cmd("Oil")  -- Open oil
+      end
+    end, { desc = "Explorer" })
   },
 }
