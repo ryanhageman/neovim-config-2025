4dd8111fa842b2beb6b0a18ec68f924a93ba2a78
Ryan Hageman <ryanhageman@icloud.com>
2025-07-27 12:10:30 -0500

    feat: add and update keymaps
    terminal movements
    window management

--- lua/ryan/keymaps.lua
+++ lua/ryan/keymaps.lua
@@ -6,9 +6,6 @@ local function buf_only()
   vim.cmd("silent! %bd|e#|bd#")
 end
 
--- Vim Settings (vimrc)
-vim.keymap.set("n", "<leader>ve", ":e $MYVIMRC<cr>", { desc = "Edit vimrc" })
-
 vim.keymap.set("n", "<leader>q", ":q<cr>", { desc = "Quit" })
 
 -- Buffer navigation
@@ -16,12 +13,11 @@ vim.keymap.set("n", "<leader>bp", ":bp<cr>", { desc = "Previous buffer" })
 vim.keymap.set("n", "<leader>bn", ":bn<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>bd", ":bd<cr>", { desc = "Delete buffer" })
 vim.keymap.set("n", "<leader><tab>", "<c-^>", { desc = "Swap with alternate buffer" })
-
-
 vim.keymap.set("n", "<leader>bo", buf_only, { desc = "Close all buffers except current" })
 
 -- Files
 vim.keymap.set("n", "<leader>fs", ":write<cr>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>fS", ":wa<cr>", { desc = "Save all files" })
 vim.keymap.set("n", "<leader>ff", ":e ", { desc = "Find file" })
 
 -- Windows
@@ -38,10 +34,11 @@ vim.keymap.set("n", "<c-l>", "<c-w><c-l>", { desc = "Move to window right" })
 
 -- Terminal
 vim.keymap.set("n", "<leader>'", ":term<cr>", { desc = "Open terminal" })
+vim.keymap.set("t", "<c-w>n", "<c-\\><c-n>", { desc = "Normal mode (terminal)" })
 
 -- Terminal mode navigation
-vim.keymap.set("t", "<c-j>", "<c-w>j", { desc = "Move to window below" })
-vim.keymap.set("t", "<c-k>", "<c-w>k", { desc = "Move to window above" })
-vim.keymap.set("t", "<c-h>", "<c-w>h", { desc = "Move to window left" })
-vim.keymap.set("t", "<c-l>", "<c-w>l", { desc = "Move to window right" })
+vim.keymap.set("t", "<c-j>", "<c-\\><c-n><c-w>j", { desc = "Move to window below" })
+vim.keymap.set("t", "<c-k>", "<c-\\><c-n><c-w>k", { desc = "Move to window above" })
+vim.keymap.set("t", "<c-h>", "<c-\\><c-n><c-w>h", { desc = "Move to window left" })
+vim.keymap.set("t", "<c-l>", "<c-\\><c-n><c-w>l", { desc = "Move to window right" })
 
