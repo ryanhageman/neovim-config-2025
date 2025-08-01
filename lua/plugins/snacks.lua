ac9380a94a20c2f8a5dfab09631ea920df63d850
Ryan Hageman <ryanhageman@icloud.com>
2025-07-31 20:27:16 -0500

    feat: update dashboard art

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,7 +5,7 @@ return {
 
   opts = {
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = require("ryan.utils.dashboard"),
     explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
---
+++ lua/ryan/utils/dashboard.lua
@@ -0,0 +1,24 @@
+local art = require("ryan.utils.dashboard-art")
+
+local settings = {
+	preset = {
+		header = art.jolly_roger(),
+		keys = {
+      { icon = "🏄", key = "p", desc = "Projects", action = ":lua Snacks.picker.projects()" },
+			{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+			{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+			{
+				icon = " ",
+				key = "c",
+				desc = "Config",
+				action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+			},
+			{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+			{ icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+			{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+		},
+	},
+}
+
+return settings
+
