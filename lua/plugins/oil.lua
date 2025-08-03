05000e343440be3afd5862004804accd8f3728b2
Ryan Hageman <ryanhageman@icloud.com>
2025-08-03 12:12:59 -0500

    feat: show hidden files in oil by default

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,10 @@
 return {
 	"stevearc/oil.nvim",
-	opts = {},
+	opts = {
+    view_options = {
+      show_hidden = true
+    }
+  },
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	lazy = false,
 	keys = {
