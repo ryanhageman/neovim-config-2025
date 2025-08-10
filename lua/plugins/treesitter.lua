8fb06be665f4b44c794e08c0f44981e466158f4c
Ryan Hageman <ryanhageman@icloud.com>
2025-08-10 12:10:05 -0500

    feat: enable treesitter markdown code block highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,16 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "main",
+	branch = "master",
 	lazy = false,
 	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+		})
+	end,
 }
