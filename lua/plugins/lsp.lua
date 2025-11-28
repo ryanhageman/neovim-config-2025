84ed0b6dc6b3740c38c951aba9d88ade50ca2d07
Ryan Hageman <ryanhageman@icloud.com>
2025-11-27 22:28:39 -0600

    refactor: lsp config to use new syntax

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,25 +26,56 @@ return {
 		"neovim/nvim-lspconfig",
 		dependencies = { "mason-org/mason-lspconfig.nvim" },
 		config = function()
-			local lspconfig = require("lspconfig")
-
 			-- Ruby LSP (without Rubocop integration)
-			lspconfig.ruby_lsp.setup({})
+			vim.lsp.enable("ruby_lsp")
 
 			-- Lua LSP
-			lspconfig.lua_ls.setup({
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" }, -- Recognize 'vim' global in Neovim configs
+			vim.lsp.config("lua_ls", {
+				on_init = function(client)
+					if client.workspace_folders then
+						local path = client.workspace_folders[1].name
+						if
+							path ~= vim.fn.stdpath("config")
+							and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+						then
+							return
+						end
+					end
+
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+						runtime = {
+							-- Tell the language server which version of Lua you're using (most
+							-- likely LuaJIT in the case of Neovim)
+							version = "LuaJIT",
+							-- Tell the language server how to find Lua modules same way as Neovim
+							-- (see `:h lua-module-load`)
+							path = {
+								"lua/?.lua",
+								"lua/?/init.lua",
+							},
 						},
+						-- Make the server aware of Neovim runtime files
 						workspace = {
-							library = vim.api.nvim_get_runtime_file("", true), -- Make LSP aware of Neovim runtime
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME,
+								-- Depending on the usage, you might want to add additional paths
+								-- here.
+								-- '${3rd}/luv/library'
+								-- '${3rd}/busted/library'
+							},
+							-- Or pull in all of 'runtimepath'.
+							-- NOTE: this is a lot slower and will cause issues when working on
+							-- your own configuration.
+							-- See https://github.com/neovim/nvim-lspconfig/issues/3189
+							-- library = {
+							--   vim.api.nvim_get_runtime_file('', true),
+							-- }
 						},
-						telemetry = {
-							enable = false, -- Disable telemetry
-						},
-					},
+					})
+				end,
+				settings = {
+					Lua = {},
 				},
 			})
 
