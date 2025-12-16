191298ec0815e9f56709532bd95d78d05d0090f5
Ryan Hageman <ryanhageman@icloud.com>
2025-12-15 22:01:35 -0600

    refactor: freshen lsp config
    - get rid of mason-lspconfig
      * don't need autoinstall
      * don't need ensure installed
    - add on_attach function for:
      * defaults
      * keymaps
    - add svelte lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,36 +1,41 @@
 return {
-	-- Mason: Install and manage language servers
 	{
 		"mason-org/mason.nvim",
 		opts = {},
 	},
 
-	-- Bridge between Mason and lspconfig
-	{
-		"mason-org/mason-lspconfig.nvim",
-		opts = {},
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "ruby_lsp", "lua_ls" },
-				automatic_installation = true,
-			})
-		end,
-	},
-
-	-- LSP configurations
 	{
 		"neovim/nvim-lspconfig",
-		dependencies = { "mason-org/mason-lspconfig.nvim" },
 		config = function()
-			-- Ruby LSP (without Rubocop integration)
-			vim.lsp.enable("ruby_lsp")
+			------------------------------------------------------------------
+			-- shared on_attach
+			------------------------------------------------------------------
+			local on_attach = function(client, buf)
+				client.server_capabilities.documentFormattingProvider = false
+				client.server_capabilities.documentRangeFormattingProvider = false
 
+				-- keymaps
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = buf, desc = "Hover" })
+				vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = buf, desc = "Rename" })
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = buf, desc = "Action" })
+				vim.keymap.set("n", "<leader>ck", vim.lsp.buf.signature_help, { buffer = buf, desc = "Signature Help" })
+			end
+
+			------------------------------------------------------------------
+			-- Ruby LSP
+			------------------------------------------------------------------
+			vim.lsp.enable("ruby_lsp")
+			vim.lsp.config("ruby_lsp", {
+				on_attach = on_attach,
+			})
+
+			------------------------------------------------------------------
 			-- Lua LSP
+			------------------------------------------------------------------
+			vim.lsp.enable("lua_ls")
 			vim.lsp.config("lua_ls", {
+				on_attach = on_attach,
+
 				on_init = function(client)
 					if client.workspace_folders then
 						local path = client.workspace_folders[1].name
@@ -44,49 +49,46 @@ return {
 
 					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
 						runtime = {
-							-- Tell the language server which version of Lua you're using (most
-							-- likely LuaJIT in the case of Neovim)
 							version = "LuaJIT",
-							-- Tell the language server how to find Lua modules same way as Neovim
-							-- (see `:h lua-module-load`)
 							path = {
 								"lua/?.lua",
 								"lua/?/init.lua",
 							},
 						},
-						-- Make the server aware of Neovim runtime files
 						workspace = {
 							checkThirdParty = false,
 							library = {
 								vim.env.VIMRUNTIME,
-								-- Depending on the usage, you might want to add additional paths
-								-- here.
-								-- '${3rd}/luv/library'
-								-- '${3rd}/busted/library'
 							},
-							-- Or pull in all of 'runtimepath'.
-							-- NOTE: this is a lot slower and will cause issues when working on
-							-- your own configuration.
-							-- See https://github.com/neovim/nvim-lspconfig/issues/3189
-							-- library = {
-							--   vim.api.nvim_get_runtime_file('', true),
-							-- }
 						},
 					})
 				end,
+
 				settings = {
-					Lua = {},
+					Lua = {
+						diagnostics = {
+							globals = { "vim", "Snacks" },
+						},
+					},
 				},
 			})
 
-			-- LSP keymaps (only for actions NOT handled by Snacks)
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf, desc = "Hover" })
-					vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename" })
-					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = ev.buf, desc = "Action" })
-				end,
+			------------------------------------------------------------------
+			-- Svelte LSP
+			------------------------------------------------------------------
+			vim.lsp.enable("svelte")
+			vim.lsp.config("svelte", {
+				on_attach = on_attach,
+
+				settings = {
+					svelte = {
+						plugin = {
+							svelte = {
+								enable = true,
+							},
+						},
+					},
+				},
 			})
 		end,
 	},
