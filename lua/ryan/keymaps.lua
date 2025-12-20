ac3a5b4c9fb60f5b01b701fbfca9f55d43fc0560
Ryan Hageman <ryanhageman@icloud.com>
2025-12-20 08:56:43 -0600

    feat: keymaps for yank and centering
    - <leader>y or Y yanks to the system clipboard
    - page up and down center
    - n/N center after jump

--- lua/ryan/keymaps.lua
+++ lua/ryan/keymaps.lua
@@ -8,12 +8,23 @@ end
 
 vim.keymap.set("n", "<leader>q", ":q<cr>", { desc = "Quit" })
 
+-- Yank/Paste
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>Y", "<cmd>%y+<cr>", { desc = "Yank the buffer to system clipboard" })
+vim.keymap.set({ "n", "x" }, "<leader>p", [["0p]], { desc = "paste from yank register" })
+
 -- Buffer navigation
 vim.keymap.set("n", "<leader>bp", ":bp<cr>", { desc = "Previous buffer" })
 vim.keymap.set("n", "<leader>bn", ":bn<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>bd", ":bd<cr>", { desc = "Delete buffer" })
 vim.keymap.set("n", "<leader><tab>", "<c-^>", { desc = "Swap with alternate buffer" })
 vim.keymap.set("n", "<leader>bo", buf_only, { desc = "Close all buffers except current" })
+
+-- Center after movement
+vim.keymap.set("n", "<c-u>", "<c-u>zz")
+vim.keymap.set("n", "<c-d>", "<c-d>zz")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
 
 -- Files
 vim.keymap.set("n", "<leader>fs", ":write<cr>", { desc = "Save file" })
